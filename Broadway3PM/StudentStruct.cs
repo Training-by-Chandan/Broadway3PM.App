66bb93613912f3444986714acac4d024243f7a9b
Chandan Gupta Bhagat <Chandan.Bhagat@outlook.com>
2021-07-11 16:15:51 +0545

    struct vs class explained

---
+++ Broadway3PM/StudentStruct.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Broadway3PM
+{
+    //https://www.c-sharpcorner.com/blogs/difference-between-struct-and-class-in-c-sharp follow this link for more information
+    public struct StudentStruct
+    {
+        public string Name;
+        public string Email;
+        public int Age;
+    }
+
+    public class StudentCls
+    {
+        public StudentCls()
+        {
+        }
+
+        public string Name;
+        public string Email;
+        public int Age;
+    }
+}
